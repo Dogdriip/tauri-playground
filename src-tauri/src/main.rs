1edadd17e7b659d38f7925bea182d2b9680ed567
Hyunseung Jeon <dogdriip@gmail.com>
2023-03-08 17:40:53 +0900

    feat(src-tauri): add builder setup for blur

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,24 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+use window_vibrancy::{apply_blur, apply_acrylic, apply_mica, apply_vibrancy, NSVisualEffectMaterial};
+
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      let window = app.get_window("main").unwrap();
+
+      #[cfg(target_os = "macos")]
+      apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+        .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+      #[cfg(target_os = "windows")]
+      apply_blur(&window, Some((18, 18, 18, 125)))
+        .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
