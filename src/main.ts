105b4f5e7418f7dfec12ae3693ffeb1c497e72aa
Hyunseung Jeon <dogdriip@gmail.com>
2023-03-09 12:56:04 +0900

    chore: configure vite-ts-vanila default settings

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
 import './style.css'
-import { setupCounter } from './counter'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <h1>Welcome from Tauri!</h1>
+    <p>${window.navigator.userAgent}</p>
   </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
