67b01e28837d868df598163ab57ee643ea0410d1
Hyunseung Jeon <dogdriip@gmail.com>
2023-03-09 13:08:46 +0900

    feat(src-tauri): apply window_shadows::set_shadow

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,20 +4,27 @@
 )]
 
 use tauri::Manager;
-use window_vibrancy::{apply_blur, apply_acrylic, apply_mica, apply_vibrancy, NSVisualEffectMaterial};
+use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
+use window_shadows::set_shadow;
 
 fn main() {
   tauri::Builder::default()
     .setup(|app| {
       let window = app.get_window("main").unwrap();
 
-      #[cfg(target_os = "macos")]
-      apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
-        .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+      #[cfg(target_os = "macos")] {
+        apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+          .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+        set_shadow(&window, true)
+          .expect("error while window_shadows::set_shadow");
+      }
 
-      #[cfg(target_os = "windows")]
-      apply_blur(&window, Some((18, 18, 18, 125)))
-        .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+      #[cfg(target_os = "windows")] {
+        apply_blur(&window, Some((18, 18, 18, 125)))
+          .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+        set_shadow(&window, true)
+          .expect("error while window_shadows::set_shadow");
+      }
 
       Ok(())
     })
