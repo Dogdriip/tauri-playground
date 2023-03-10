5cf88eba1de66905144caf078fc1d7f8767069ad
Hyunseung Jeon <dogdriip@gmail.com>
2023-03-10 15:14:30 +0900

    chore(src): remove top-level await

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { invoke } from '@tauri-apps/api'
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Welcome from Tauri!</h1>
+(async function() {
+  document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+    <div>
+      <h1>Welcome from Tauri!</h1>
 
-    <h2>From JS:</h2>
-    <p>${window.navigator.userAgent}</p>
+      <h2>From JS:</h2>
+      <p>${window.navigator.userAgent}</p>
 
-    <h2>From Rust:</h2>
-    <p>${await invoke('arch')}</p>
-    <p>${await invoke('family')}</p>
-    <p>${await invoke('os')}</p>
-  </div>
-`
+      <h2>From Rust:</h2>
+      <p>${await invoke('arch')}</p>
+      <p>${await invoke('family')}</p>
+      <p>${await invoke('os')}</p>
+    </div>
+  `
+})()
